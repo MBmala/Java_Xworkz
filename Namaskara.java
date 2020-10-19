3d498a3391cb57be8225955f0f4aa7e30f823e89
MBmala <72956567+MBmala@users.noreply.github.com>
2020-10-19 21:14:00 +0530

    simple java program

---
+++ Namaskara.java
@@ -0,0 +1,16 @@
+class Wish
+{
+//main method is entry point to run sourcecode
+public static void main(String[] mala)
+{
+System.out.println("Namaskara");
+}
+}
+class Camera
+{
+public static void main(String[] mala)
+{
+System.out.println("hi");
+}
+
+}
