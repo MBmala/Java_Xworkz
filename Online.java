279ae896b2a0ea2f74e07e7302198c776bf322c7
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-23 07:59:05 +0530

    Add files via upload
    java program on passing argument to the main method.

---
+++ Online.java
@@ -0,0 +1,15 @@
+public class Online{
+public static void main(String[] trainees)
+{
+	System.out.println("JVM invoked main");
+	//print refernce representation
+	System.out.println("trainees "+ trainees);
+	
+	int sizeOfMainParam=trainees.length;
+	System.out.println("sizeOfMainParam " +sizeOfMainParam);
+	//pass arguments while running ie :java Online mala apeksha madhu 
+	System.out.println("Element @ index 0 "+trainees[0]);
+	System.out.println("Element @ index 1 "+trainees[1]);
+	System.out.println("exit main");
+}
+}
