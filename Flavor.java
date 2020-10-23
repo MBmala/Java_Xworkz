f5b15446579f018bdec7b309ce90748f76131722
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-23 22:54:59 +0530

    Add files via upload

---
+++ Flavor.java
@@ -0,0 +1,8 @@
+public class Flavor{
+  public static void taste(String name){
+   System.out.println("Sweet name: "+name);
+   //System.out.println("element at index 1"+name);
+   //System.out.println("element at index 2"+name);
+  
+}
+}
