94d197528949c526347d9d568fac4f2b9ffb4356
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 11:14:44 +0530

    Add files via upload
    java program to print message as welcome to Xworkz.

---
+++ Greet.java
@@ -0,0 +1,9 @@
+public class Greet{
+  public static void main(String[] args){
+	printMsg("Welcome");  
+  }
+  public static void printMsg(String wc)
+  {
+	  System.out.println(wc+" to X-workz");
+  }
+}
