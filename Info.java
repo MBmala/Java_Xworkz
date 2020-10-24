885834bdb6db7425c8a9ca0255bfd8f479b39ae5
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 11:13:23 +0530

    Add files via upload

---
+++ Info.java
@@ -0,0 +1,13 @@
+public class Info{
+   public static void main(String[] params)
+   {
+	email("mala.xworkx@gmail.com");
+    programmingLang("Java");	
+   }
+   public static void email(String id){
+	   System.out.println("Email address:"+id);
+   }
+   public static void programmingLang(String name){
+	   System.out.println("Programming Language:"+name);
+   }
+}
