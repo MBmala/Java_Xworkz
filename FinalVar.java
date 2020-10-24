c544638bf069fa25ed4a97e8a865da9a64309aff
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 11:17:51 +0530

    Add files via upload
    variable is declared with keyword final ,means its value is constant ,it won't change.

---
+++ FinalVar.java
@@ -0,0 +1,11 @@
+public class FinalVar{
+	public static void main(String[] args){
+		
+		final long adharNo=951974281786L;
+		System.out.println("Adhar number:"+adharNo);
+		final long mobile=7829008765L;
+		System.out.println("Phone number:"+mobile);
+		final String email="mala.xworkz@gmail.com";
+		System.out.println("Email id:"+email);
+	}
+}
