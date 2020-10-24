6b71d32be3348f8103a1e70e8d40c11d3090844a
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 10:51:09 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,14 @@
+class Hello{
+public static void main(String[] params)
+{
+music();
+drinking();
+}
+
+public static void music(){
+System.out.println("When music hits you,you feel no pain");
+}
+public static void drinking(){
+System.out.println("Drink lot of water stay hydrated");
+}
+}
