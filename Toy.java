04a95c165c1473ac99bc5e4b5520ef23da5e6944
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 10:55:08 +0530

    Delete Toy.java

--- Toy.java
+++
@@ -1,7 +0,0 @@
-public class Toy{
-	public static void main(String[] params){
-		System.out.println("JVM invokes main method");
-		Pubg.enimiesAhed();//Calling method of another class(ie.Pubg)
-		System.out.println("End of main");
-	}
-}
