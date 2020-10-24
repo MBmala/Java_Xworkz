819c1167f84a9ae9b202eb52e28325f0e4c14056
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 10:50:07 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class MethodDemo{
+public static void main(String[] params)
+{
+write();
+write();
+write();
+}
+ public static void write(){
+System.out.println("aspire to inspire before we expire");
+}
+}
