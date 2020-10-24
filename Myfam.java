241f58ee7db911685c305fc8ef482f0c0ec877f6
Balehosur Mala <72956567+MBmala@users.noreply.github.com>
2020-10-24 10:52:06 +0530

    Add files via upload

---
+++ Myfam.java
@@ -0,0 +1,26 @@
+class Family{
+public static void main(String[] args)
+{
+System.out.println("My name is Mala");
+father();
+mother();
+brother();
+sister();
+}
+ static void father()
+{
+System.out.println("My father name is Mouneshwar");
+}
+ static void mother()
+{
+System.out.println("My mother name is Lalita");
+}
+static void brother()
+{
+System.out.println("My bro name is Omkar");
+}
+static void sister()
+{
+System.out.println("My sis name is Madhu");
+}
+}
